56423f6cf98b08ebbcef43e587858110ecbc6db5
Michalie Mazurkivich <michaliem3@gmail.com>
2020-09-24 04:05:37 +0000

    completed homework4

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,20 +1,53 @@
-# Author: Yanling Wang yuw17@psu.edu
-def getGradePoint(letter):
-  """
-  Given a letter grade, translate it into a gradepoint value between 0.0 and 4.0
-  YOU MUST USE if/elif/../else structure to implement this function.
-  YOU CAN NOT use any features/data structure we haven't learned in the class,
-  including but not limited to list, dictionary and tuples.
-  """
+# Author: Michalie Mazurkivich mpm6778@psu.edu
+
+def getGradePoint(letterGrade):
+  if letterGrade == 'A':
+    gradePointNum = 4.0
+  elif letterGrade == 'A-':
+    gradePointNum = 3.67
+  elif letterGrade == 'B+':
+    gradePointNum = 3.33
+  elif letterGrade == 'B':
+    gradePointNum = 3.0
+  elif letterGrade == 'B-':
+    gradePointNum = 2.67
+  elif letterGrade == 'C+':
+    gradePointNum = 2.33
+  elif letterGrade == 'C':
+    gradePointNum = 2.0
+  elif letterGrade == 'D':
+    gradePointNum = 1.0
+  else:
+    gradePointNum = 0.0
+  gradePointNum = float(gradePointNum)
+  return(gradePointNum)
   return 0.0
 
+def calcGPA(gradepoint1, credit1, gradepoint2, credit2, gradepoint3, credit3):
+  GPA = (gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3)
+  return(GPA)
+
 def run():
-  """
-  Use either a while-loop or a for-loop to make your code for getting three separate
-  courses' letter grade and credit info. And calculate the final GPA.
-  YOU CAN NOT use any features/data structures we haven't learned in the class,
-  including but not limited to list, dictionary, and tuples.
-  """
+  n=1
+  while (n<=3):
+    letterGrade = input(f"Enter your course {n} letter grade: ")
+    credit = input(f"Enter your course {n} credit: ")
+    gradepoint = getGradePoint(letterGrade)
+    print (f"Grade point for course {n} is: {gradepoint}")
+    credit = float(credit)
+    if (n==1):
+      gradepoint1 = gradepoint
+      credit1 = credit
+    elif (n==2):
+      gradepoint2 = gradepoint
+      credit2 = credit
+    elif (n==3):
+      gradepoint3 = gradepoint
+      credit3 = credit
+    n = n+1
+
+  GPA = calcGPA (gradepoint1, credit1, gradepoint2, credit2, gradepoint3, credit3)
+  print (f"Your GPA is: {GPA}")
   return
 
 if __name__ == "__main__":
